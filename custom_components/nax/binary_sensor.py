be3a1511c8b69c974ecd5bae3dac4232bae70bd9
sshanafelt <sshanafelt@jetsonsys.com>
2025-09-16 13:43:58 -0400

    added basic HA device logic

--- custom_components/nax/binary_sensor.py
+++ custom_components/nax/binary_sensor.py
@@ -10,7 +10,7 @@ from homeassistant.components.binary_sensor import BinarySensorEntity
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant, callback
 from homeassistant.exceptions import ConfigEntryNotReady
-from homeassistant.helpers.device_registry import CONNECTION_NETWORK_MAC, format_mac
+from homeassistant.helpers.device_registry import format_mac
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from .const import DOMAIN
@@ -27,7 +27,6 @@ async def async_setup_entry(
     """Set up NAX sensor entities for a config entry."""
 
     api: DataEventManager = hass.data[DOMAIN][config_entry.entry_id]
-    # mac_address = await hass.async_add_executor_job(api.get_device_mac_address)
 
     mac_address = (
         (await api.client.http_get("/Device/DeviceInfo/MacAddress") or {})
@@ -36,6 +35,31 @@ async def async_setup_entry(
         .get("DeviceInfo", {})
         .get("MacAddress")
     )
+
+    nax_device_name = (
+        (await api.client.http_get("/Device/DeviceInfo/Name") or {})
+        .get("content", {})
+        .get("Device", {})
+        .get("DeviceInfo", {})
+        .get("Name")
+    )
+
+    nax_device_manufacturer = (
+        (await api.client.http_get("/Device/DeviceInfo/Manufacturer") or {})
+        .get("content", {})
+        .get("Device", {})
+        .get("DeviceInfo", {})
+        .get("Manufacturer")
+    )
+
+    nax_device_model = (
+        (await api.client.http_get("/Device/DeviceInfo/Model") or {})
+        .get("content", {})
+        .get("Device", {})
+        .get("DeviceInfo", {})
+        .get("Model")
+    )
+
     source_inputs = (
         (await api.client.http_get("/Device/InputSources/Inputs") or {})
         .get("content", {})
@@ -49,9 +73,12 @@ async def async_setup_entry(
         raise ConfigEntryNotReady("NAX device not available")
 
     entities_to_add = [
-        NaxInputSignalSensor(
+        NaxInputSignalBinarySensor(
             api=api,
             mac_address=mac_address,
+            nax_device_name=nax_device_name,
+            nax_device_manufacturer=nax_device_manufacturer,
+            nax_device_model=nax_device_model,
             source_input_key=source_input,
             source_input_data=source_inputs[source_input],
         )
@@ -61,18 +88,27 @@ async def async_setup_entry(
     async_add_entities(entities_to_add)
 
 
-class NaxInputSignalSensor(NaxEntity, BinarySensorEntity):
+class NaxInputSignalBinarySensor(NaxEntity, BinarySensorEntity):
     """Representation of a NAX Input Signal Sensor."""
 
     def __init__(
         self,
         api: DataEventManager,
         mac_address: str,
+        nax_device_name: str,
+        nax_device_manufacturer: str,
+        nax_device_model: str,
         source_input_key: str,
         source_input_data: dict,
     ) -> None:
         """Initialize the sensor."""
-        super().__init__(api=api, mac_address=mac_address)
+        super().__init__(
+            api=api,
+            mac_address=mac_address,
+            nax_device_name=nax_device_name,
+            nax_device_manufacturer=nax_device_manufacturer,
+            nax_device_model=nax_device_model,
+        )
         self._source_input_key = source_input_key
         self._attr_unique_id = (
             f"{format_mac(mac_address)}_{source_input_key}_signal_present"
@@ -88,11 +124,17 @@ class NaxInputSignalSensor(NaxEntity, BinarySensorEntity):
         )
         api.subscribe(
             f"/Device/InputSources/Inputs/{self._source_input_key}/Name",
-            self._device_name_update,
+            self._input_name_update,
         )
 
     @callback
     def _is_signal_present_update(self, event_name: str, message: Any) -> None:
         """Handle updates to the signal presence."""
         self._attr_is_on = message
+        self.async_write_ha_state()
+
+    @callback
+    def _input_name_update(self, event_name: str, message: Any) -> None:
+        """Handle updates to the input name."""
+        self._attr_name = f"{message} Signal Present"
         self.async_write_ha_state()
--- custom_components/nax/nax_entity.py
+++ custom_components/nax/nax_entity.py
@@ -1,12 +1,13 @@
 """NAX base entity class for Home Assistant integration."""
 
-from collections.abc import Callable
 from typing import Any
 
 from cresnextws import ConnectionStatus, DataEventManager
 from homeassistant.core import callback
-from homeassistant.helpers.device_registry import CONNECTION_NETWORK_MAC, format_mac
+from homeassistant.helpers.device_registry import DeviceInfo
 from homeassistant.helpers.entity import Entity
+
+from .const import DOMAIN
 
 
 class NaxEntity(Entity):
@@ -16,18 +17,35 @@ class NaxEntity(Entity):
         self,
         api: DataEventManager,
         mac_address: str,
+        nax_device_name: str,
+        nax_device_manufacturer: str,
+        nax_device_model: str,
     ) -> None:
         """Initialize the entity.
 
         Args:
             api: The DataEventManager instance
             mac_address: MAC address of the device
-            subscriptions: List of tuples containing (path, callback) for data subscriptions
+            nax_device_name: Name of the device
+            nax_device_manufacturer: Manufacturer of the device
+            nax_device_model: Model of the device
         """
         self.api = api
         self.mac_address = mac_address
         self._attr_should_poll = False
         self._attr_entity_registry_visible_default = False
+
+        # Create device info
+        self._attr_device_info = DeviceInfo(
+            identifiers={(DOMAIN, mac_address)},
+            name=nax_device_name,
+            manufacturer=nax_device_manufacturer,
+            model=nax_device_model,
+        )
+        api.subscribe(
+            "/Device/DeviceInfo/Name",
+            self._nax_device_name_update,
+        )
         self.api.client.add_connection_status_handler(
             self._device_connection_status_update
         )
@@ -36,10 +54,20 @@ class NaxEntity(Entity):
         """Run when entity is added to Home Assistant."""
 
     @callback
-    def _device_name_update(self, path: str, data: Any) -> None:
+    def _nax_device_name_update(self, path: str, data: Any) -> None:
+        """Update device name when it changes."""
         if self._attr_device_info:
+            # Update the device info with new name, preserving existing info
             self._attr_device_info["name"] = data
-            self.schedule_update_ha_state(force_refresh=False)
+            # self._attr_device_info = DeviceInfo(
+            #     identifiers=self._attr_device_info.get(
+            #         "identifiers", {(DOMAIN, self.mac_address)}
+            #     ),
+            #     name=data,
+            #     manufacturer=self._attr_device_info.get("manufacturer", "Crestron"),
+            #     model=self._attr_device_info.get("model", "NAX"),
+            # )
+            self.async_write_ha_state()
 
     @callback
     def _generic_update(self, path: str, data: Any) -> None:
